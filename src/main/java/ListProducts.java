e47ba1687b083f630195607ba445e42ca2320d13
jordy muniz <jordy.muniz96@gmail.com>
2022-11-07 11:39:45 -0600

    accessing data walkthrough

---
+++ src/main/java/ListProducts.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListProducts implements Products{
+
+    private List<Product> products = new ArrayList<>();
+
+    public ListProducts(){
+        insert(new Product("hammer", 9.99));
+        insert(new Product("screwdriver", 9.99));
+        insert(new Product("drill", 19.99));
+    }
+
+    public List<Product> all() {
+       return this.products;
+    }
+
+
+    public void insert(Product product) {
+        this.products.add(product);
+    }
+}
